cfc31cda823613c8e0d889f4934d97e6cfbd3424
Fredrik Svensson <svensson.jf@gmail.com>
2026-03-09 22:48:08 +0100

    Add BudgetRepository export to repository index

--- packages/db/src/repositories/index.ts
+++ packages/db/src/repositories/index.ts
@@ -1,6 +1,7 @@
 export { OrganizationRepository } from "./organization.repository.js";
 export { VoucherRepository } from "./voucher.repository.js";
 export { VoucherTemplateRepository } from "./voucher-template.repository.js";
+export { BudgetRepository } from "./budget.repository.js";
 export { AccountRepository } from "./account.repository.js";
 export { FiscalYearRepository } from "./fiscal-year.repository.js";
 export { DocumentRepository } from "./document.repository.js";
